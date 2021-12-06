45db47b78e9aeb4f9708ac6adc93ab7c330a194a
Kyoto-01 <cameraman269@gmail.com>
2021-12-05 21:15:50 -0300

    debug process table

--- procview.sh
+++ procview.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
+procs=()
+
 # salvando informações de processos
-ps aux | tr -s " " | tail -n +2 > procs.txt
+ps aux | tr -s " " | tr -d "<>" | tail -n +2 > procs.txt
 
 # guardando informações de processos que serão usadas
-pid=$( cut -d " " -f 2 procs.txt )
-usr=$( cut -d " " -f 1 procs.txt ) 
-cpu=$( cut -d " " -f 3 procs.txt )
-mem=$( cut -d " " -f 4 procs.txt )
-vsz=$( cut -d " " -f 5 procs.txt )
-rss=$( cut -d " " -f 6 procs.txt )
-stt=$( cut -d " " -f 8 procs.txt )
-cmd=$( cut -d " " -f 11 procs.txt )
+cut -d " " -f 2 procs.txt > pid.txt
+cut -d " " -f 1 procs.txt > usr.txt
+cut -d " " -f 3 procs.txt > cpu.txt
+cut -d " " -f 4 procs.txt > mem.txt
+cut -d " " -f 5 procs.txt > vsz.txt
+cut -d " " -f 6 procs.txt > rss.txt
+cut -d " " -f 8 procs.txt > stt.txt
+cut -d " " -f 11 procs.txt > cmd.txt
+
+# formatando tabela com as informações de processos que serão usadas
+paste pid.txt usr.txt cpu.txt mem.txt vsz.txt rss.txt stt.txt cmd.txt \
+| tr "\t" "\n" > procs.txt
+
+# passando dados da tabela em texto para um array
+while read line
+do
+	procs=( ${procs[@]} ${line} )
+done < procs.txt
+
+# removendo arquivos desnescessários
+rm -f pid.txt usr.txt cpu.txt mem.txt vsz.txt rss.txt stt.txt cmd.txt procs.txt
 
 # criando GUI
 FORM=$(
@@ -20,14 +35,19 @@ FORM=$(
 	--width=700 --height=700			\
 	--window-icon="icon.ico"			\
 	--list						\
-	--column="PID":text "${pid}"			\
-	--column="User":text "${usr}"			\
-	--column="CPU":txt "${cpu}"			\
-	--column="RAM":txt "${mem}"			\
-	--column="VSZ":txt "${vsz}"			\
-	--column="RSS":txt "${rss}"			\
-	--column="Status":txt "${stt}"			\
-	--column="Comand":txt "${cmd}"			\
-	--focus-field=1					\
+	--column="PID":NUM				\
+	--column="User":TEXT 				\
+	--column="CPU(%)":FLT 				\
+	--column="RAM(%)":FLT 				\
+	--column="VSZ(KB)":NUM 				\
+	--column="RSS(KB)":NUM 				\
+	--column="Status":TEXT 				\
+	--column="Comand":TEXT 				\
+	"${procs[@]}"					\
+	--button="Filter by user":"ls"			\
+	--button="Start process":"ls"			\
+	--button="Schedule process":"ls"		\
+	--button="Process Graphic":"ls"			\
+	--button="Kill":"ls"				\
 )
 
