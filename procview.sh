c386a96b129bee1d6959eb1a86ba62e0c562a1e6
Kyoto-01 <cameraman269@gmail.com>
2021-12-14 18:35:00 -0300

    refresh with scrolling added

--- procview.sh
+++ procview.sh
@@ -1,53 +1,52 @@
 #!/bin/bash
 
-procs=()
+PID=$$
+REFRESH_TIME=1
 
-# salvando informações de processos
-ps aux | tr -s " " | tr -d "<>" | tail -n +2 > procs.txt
-
-# guardando informações de processos que serão usadas
-cut -d " " -f 2 procs.txt > pid.txt
-cut -d " " -f 1 procs.txt > usr.txt
-cut -d " " -f 3 procs.txt > cpu.txt
-cut -d " " -f 4 procs.txt > mem.txt
-cut -d " " -f 5 procs.txt > vsz.txt
-cut -d " " -f 6 procs.txt > rss.txt
-cut -d " " -f 8 procs.txt > stt.txt
-cut -d " " -f 11 procs.txt > cmd.txt
-
-# formatando tabela com as informações de processos que serão usadas
-paste pid.txt usr.txt cpu.txt mem.txt vsz.txt rss.txt stt.txt cmd.txt \
-| tr "\t" "\n" > procs.txt
-
-# passando dados da tabela em texto para um array
-while read line
+# enquanto a caixa de diálogo estiver em execução
+while [ $( pgrep -P ${PID} | wc -l ) -gt 1 ]
 do
-	procs=( ${procs[@]} ${line} )
-done < procs.txt
+	# salvando informações de processos
+	ps aux | tr -s " " | tr -d "<>" | tail -n +2 > procs.txt
 
-# removendo arquivos desnescessários
-rm -f pid.txt usr.txt cpu.txt mem.txt vsz.txt rss.txt stt.txt cmd.txt procs.txt
+	# guardando informações de processos que serão usadas
+	cut -d " " -f 2 procs.txt > pid.txt
+	cut -d " " -f 1 procs.txt > usr.txt
+	cut -d " " -f 3 procs.txt > cpu.txt
+	cut -d " " -f 4 procs.txt > mem.txt
+	cut -d " " -f 5 procs.txt > vsz.txt
+	cut -d " " -f 6 procs.txt > rss.txt
+	cut -d " " -f 8 procs.txt > stt.txt
+	cut -d " " -f 11 procs.txt > cmd.txt
 
-# criando GUI
-FORM=$(
-	yad --center					\
+	# formatando tabela com as informações de processos que serão usadas
+	paste pid.txt usr.txt cpu.txt mem.txt vsz.txt rss.txt stt.txt cmd.txt \
+	| tr "\t" "\n" > procs.txt
+	
+	cat procs.txt
+
+	sleep ${REFRESH_TIME}
+
+done | yad --center					\
 	--title="ProcView - Process Visualizer"		\
 	--width=700 --height=700			\
 	--window-icon="icon.ico"			\
+	--tail						\
 	--list						\
 	--column="PID":NUM				\
 	--column="User":TEXT 				\
 	--column="CPU(%)":FLT 				\
 	--column="RAM(%)":FLT 				\
-	--column="VSZ(KB)":NUM 				\
-	--column="RSS(KB)":NUM 				\
+	--column="VSZ":SZ				\
+	--column="RSS":SZ 				\
 	--column="Status":TEXT 				\
 	--column="Comand":TEXT 				\
-	"${procs[@]}"					\
 	--button="Filter by user":"ls"			\
 	--button="Start process":"ls"			\
 	--button="Schedule process":"ls"		\
 	--button="Process Graphic":"ls"			\
 	--button="Kill":"ls"				\
-)
+
+# removendo arquivos desnescessários
+rm -f pid.txt usr.txt cpu.txt mem.txt vsz.txt rss.txt stt.txt cmd.txt procs.txt
 
