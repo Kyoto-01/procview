baba3a9440cb9e78a2a7c0a437f3d9ab52d3bf2e
gu1lh3rm3L <93836462+gu1lh3rm3L@users.noreply.github.com>
2021-12-21 00:10:09 -0300

    Create schudle_proc.sh

---
+++ schudle_proc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+A=$( yad --form --title='Schudle Process' --field='Command:'TEXT --field='Min':NUM )
+
+COMMAND=$(echo "SA" | cut -d'|' -f1)
+MIN=$(echo "$A" | cut -d'|' -f2)
+TEMPO=$(date --date "now +${MIN%%.*} min" | egrep -o '[0-9]{2}:[0-9]{2}')
+at ${TEMPO} <<< ${COMMAND}
