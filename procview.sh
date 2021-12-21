a8088f70594d5dd7974827f3caf62259df46545c
Kyoto-01 <cameraman269@gmail.com>
2021-12-21 00:59:39 -0300

    new features added
    * start a process
    * schedule a process
    * generate resource usage chart of an process
    * filter by user

--- procview.sh
+++ procview.sh
@@ -7,17 +7,29 @@
 #
 # -----------------------------------------------  
 
-UPDATE_INTERVAL=5
-
-
 function update_procs(){
+	user=$( get_user )
+
 	# salvando informações de processos
 	ps aux | sort -rk 9 | tr -s " " | tr -d "<>" > procs.txt
 
-	# formatando informações de processos
+	# Se aplicável, implementa filtro por usuário
+	if [ "${user}" != "${START_USER}" ]; then
+		sed -n -E "/^${user}/p" procs.txt > procs.bkp.txt
+		cat procs.bkp.txt > procs.txt
+		rm -f procs.bkp.txt
+	fi
+
+	# formata para criação de tabela no yad
 	echo "$( awk -f procformat.awk < procs.txt )" > procs.txt
 
+	# retorno
 	cat procs.txt
+}
+
+function refresh_screen(){
+	echo -e "\f"
+	update_procs
 }
 
 function update_screen(){
@@ -25,11 +37,10 @@ function update_screen(){
 	do
 		if [ "$( cat update.txt )" -eq 1 ]
 		then
-			echo -e "\f"
-			update_procs
+			refresh_screen	
 			sleep ${UPDATE_INTERVAL}
 		fi
-	done
+	done 2> /dev/null
 }
 
 function set_update_screen(){
@@ -40,44 +51,66 @@ function set_update_screen(){
 }
 
 function plot_chart(){
-	# TODO: plotar gráfico
-	echo
+	echo -e "Process with ID: $3\t$5\t1\t$6\t2" | tr "," "." > chartdata.txt
+	
+	gnuplot -p < chartplot.gpi
+
+	rm -f chartdata.txt	
 }
 
 function kill_proc(){
-	# TODO: matar processo
+	pid=$( echo "$1" | tr "|" "\n" | head -1 )
+	
+	kill -9 ${pid}
 	./$0
+}
+
+function get_user(){
+	echo "$( awk '$1="users" { print $2 }' run.conf )"
+}
+
+function set_user(){
+	sed "/user\t.*/c user\t$1" run.conf > run.conf.bkp
+	cat run.conf.bkp > run.conf
+	rm -f run.conf.bkp
+}
+
+function filter_by_user(){
+	set_user $( ./setuser.sh )	
 }
 
 function main(){
 
 	export -f set_update_screen
 	export -f plot_chart
-	export -f kill_proc
+	export -f set_user
+	export -f filter_by_user
 
 	set_update_screen 1
 
-	GUI=$( yad --list				\
-	--title="ProcView - Process Viewer"		\
-	--width=700 --height=700 --center --on-top	\
-	--window-icon="icon.ico"			\
-	--dclick-action="bash -c 'plot_chart %s'"	\
-	--grid-lines="both"				\
-	--column="@fore@"				\
-	--column="@back@"				\
-	--column="PID:NUM"				\
-	--column="User:TEXT" 				\
-	--column="CPU(%):FLT" 				\
-	--column="RAM(%):FLT" 				\
-	--column="VSZ:SZ"				\
-	--column="RSS:SZ" 				\
-	--column="Start:TEXT" 				\
-	--column="Comand:TEXT"				\
-	--button="Stop:bash -c 'set_update_screen 0'" 	\
-	--button="Start:bash -c 'set_update_screen 1'"	\
-	--button="Filter by user:ls"			\
-	--button="Start process:ls"	    		\
-	--button="Schedule process:ls"			\
+	GUI=$( yad --list					\
+	--title="ProcView - Process Viewer"			\
+	--width=700 --height=700 --center			\
+	--window-icon="icon.ico"				\
+	--no-escape						\
+	--dclick-action="bash -c 'plot_chart %s'"		\
+	--grid-lines="both"					\
+	--column="@fore@"					\
+	--column="@back@"					\
+	--column="PID:NUM"					\
+	--column="User:TEXT" 					\
+	--column="CPU(%):FLT" 					\
+	--column="RAM(%):FLT" 					\
+	--column="VSZ:SZ"					\
+	--column="RSS:SZ" 					\
+	--column="Start:TEXT" 					\
+	--column="Comand:TEXT"					\
+	--buttons-layout="center"				\
+	--button="Stop:bash -c 'set_update_screen 0'" 		\
+	--button="Start:bash -c 'set_update_screen 1'"		\
+	--button="Filter by user:bash -c 'filter_by_user'"	\
+	--button="Start process:bash -c './startproc.sh'"	\
+	--button="Schedule process:bash -c './schedproc.sh'"	\
 	--button="Kill:0" < <( update_screen )&
 	)
 
@@ -87,8 +120,15 @@ function main(){
 	# Se saída da GUI for não vazia, um processo 
 	# foi especificado para ser morto.
 	# Senão, o processo atual será morto 
-	[ -n "${GUI}" ] && kill_proc ${GUI}
+	[ -n "${GUI}" ] && kill_proc "${GUI}" 2> /dev/null
+
+	# retornando ao user padrão
+	set_user "${START_USER}"
 }
 
-main &> /dev/null
+
+UPDATE_INTERVAL=5
+START_USER=$( get_user )
+
+main
 
